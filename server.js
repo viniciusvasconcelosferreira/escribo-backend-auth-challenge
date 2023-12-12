2942dd09bf9a654ac2e0a16a82493d8a3b713e53
Vinicius Vasconcelos Ferreira <viniciusvasconcelos.ferreira@gmail.com>
2023-12-11 21:39:01 -0300

    feat: configura eslint e prettier para padronização do código
    BREAKING CHANGE: define os padrões de estilização e indentação do código, configura o sucrase para melhora do desenvolvimento em ambiente local e acrescenta novos arquivos a serem ignorados

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', function (req, res) {
-    res.send('Bem-Vindo ao Express !');
+  res.send('Bem-Vindo ao Express !');
 });
 
 app.listen(port, function () {
-    console.log(`Servidor escutando na porta ${port}`);
+  console.log(`Servidor iniciado em http://localhost:${port}`);
 });
