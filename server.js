b74b700619ac2cb640b33ee24db819f6a74627f2
Vinicius Vasconcelos Ferreira <viniciusvasconcelos.ferreira@gmail.com>
2023-12-13 00:42:38 -0300

    feat: preparando para deploy

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.listen(port, function() {
-  console.log(`Servidor iniciado em http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
